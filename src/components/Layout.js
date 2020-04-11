539023b800b045942e458e1a06dc36a703a83995
chunxia.zheng <chunxia.zheng@promote.cache-dns.local>
2020-04-12 00:11:52 +0800

    最大宽度

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,7 +13,7 @@ const TemplateWrapper = ({ children, errors, layoutProps }) => {
     console.error("page error: ", errors);
   }
   return (
-    <div style={{ margin: "0 auto" }}>
+    <div style={{ margin: "0 auto", maxWidth: "750px" }}>
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
