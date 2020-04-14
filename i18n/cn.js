848600615d2276304ce15c8a6694f39c0e257ccd
jianghe1220 <62862171+jianghe1220@users.noreply.github.com>
2020-04-14 15:11:56 +0800

    Create cn.js

---
+++ i18n/cn.js
@@ -0,0 +1,17 @@
+const translations = {
+
+  courseOperationStatus: {
+    onlineCourse: '网课',
+    normalCourse: '正常上课',
+    holiday: '放假',
+    suspendCourse: '停课',
+  },
+
+  bingAreaId: {
+    newyork_unitedstates: '纽约州',
+    pennsylvania_unitedstates: '宾夕法尼亚州',
+    michigan_unitedstates: '密歇根州',
+  },
+};
+
+export default translations;
